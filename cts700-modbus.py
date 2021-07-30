a58ca3b788e72d74fed3296a0a535d2950d90689
wernerfred <20406381+wernerfred@users.noreply.github.com>
2021-07-30 11:59:19 +0200

    refactor: unit conversion and mapping

--- cts700-modbus.py
+++ cts700-modbus.py
@@ -2,30 +2,29 @@
 
 from pymodbus.client.sync import ModbusTcpClient
 
+registerMapping = {
+    "outdoor_temp":       [20282, 0.1],
+    "indoor_temp":        [20286, 0.1],
+    "indoor_temp_wanted": [20260, 0.1],
+    "bypass_state":       [21773, 1],
+    "water_temp_bottom":  [20522, 0.11],
+    "water_temp_wanted":  [20460, 0.11],
+    "humidity_average":   [20164, 1],
+}
 
-def readRegister(register, slaveID):
+def readRegister(register, slaveID, unitConversionFactor):
     try:
-        result = client.read_holding_registers(register, 1, unit=slaveID)
-        print(result.registers)
+        response = client.read_holding_registers(register, 1, unit=slaveID)
+        calcUnit = round(response.registers[0] * unitConversionFactor, 2)
+        return calcUnit
     except:
-        print("Error reading register")
+        print("Error reading register: " + register)
 
 
 client = ModbusTcpClient('10.10.10.90', port=502)
-client.connect()
 
-print("Outdoor temp:")
-readRegister(20282, 1)  # T1 outdoor air temperature (C)
-print("Room temp:")
-readRegister(20286, 1)  # T3 extract air, room temperature (C)
-print("Room temp wanted:")
-readRegister(20260, 1)  # Wanted room temperature (C)
-print("Bypass:")
-readRegister(21773, 1)  # Bypass damper
-print("Water temp:")
-readRegister(20522, 1)  # T12 bottom temperature in DHW water tank(C)
-print("Water temp wanted:")
-readRegister(20460, 1)  # Hot water set point (C)
-print("Humidity:")
-readRegister(20164, 1)  # Average Humidity (%)
-client.close()
+for key in registerMapping:
+    client.connect()
+    print(key + ": " +
+          str(readRegister(registerMapping[key][0], 1, registerMapping[key][1])))
+    client.close()
